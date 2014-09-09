dcebfb8c4aa270fd837105b72b84bb1e9b2c273d
sebaschan <tebusco@Sebastian-R.local>
2014-09-09 15:09:38 -0500

    ftg
    fg

---
+++ desarrollo web/index.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:fb="http://www.facebook.com/2008/fbml">
+<!--<html xmlns:fb="http://ogp.me/ns/fb#">-->
+  <head>
+    <title>1001Carros.com</title>
+    <style>
+      body {
+        font-family: 'Lucida Grande', Verdana, Arial, sans-serif;
+		margin:0px;
+		padding:0px;
+      }
+	  .loginbt, .regbt{
+		  position:absolute;
+		  left:540px;
+	  }
+	  .regbt{
+		  top:470px;
+	  }
+	  .loginbt{
+		  top:520px;
+	  }
+    </style>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js" type="text/javascript"></script>
+  </head>
+  <body>
+    
+  <img src="nofan.jpg" width="810" height="600">
+    <div class="loginbt"><a href="login.php"><img src="logbt.png"></a></div>
+  <div class="regbt"><a href="registro.php"><img src="regbt.png"></a></div>
+   
+</body>
+</html>
